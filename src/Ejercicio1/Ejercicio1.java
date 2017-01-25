a9e02c034a98e6c405f79c573887f851b83685a8
1dam.m <1dam.m@PC112.cenecmalaga.net>
2017-01-25 13:03:56 +0100

    EJERCICIO 1 AHA

--- src/Ejercicio1/Ejercicio1.java
+++ src/Ejercicio1/Ejercicio1.java
@@ -1,10 +1,51 @@
 package Ejercicio1;
 
+import java.util.*;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		float centi = Validacion();
+		float fahren = Centiafahren(centi);
+		
+		System.out.println(centi + " centígrados vale " + fahren + " en fahrenheit");
+	}
+	
+	public static float Validacion(){
+		Scanner sc = new Scanner(System.in);
+		String grado;
+		float grados = 0f;
+		boolean error = true;
+		
+		do{
+			error = false;
+			System.out.print("Introduce los grados centígrados: ");
+			grado = sc.next();
+			if(tryparse(grado)){
+				grados = Float.parseFloat(grado);
+			}else{
+				error = true;
+				System.out.println("ERROR: No has introducido un número");
+			}
+			
+		}while(error);
+		
+		return grados;
+	}
+	
+	public static boolean tryparse(String grado){
+		try{
+			Float.parseFloat(grado);
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+		
+	}
+	
+	public static float Centiafahren(float centigrados){
+		return 32 + (9 * (centigrados/5));
 	}
 
 }
